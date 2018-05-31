29281a0acd6b5fda5fc79187b2c59a4763649ee0
Alissa <alissamueller@outlook.de>
2018-05-31 18:59:26 +0200

    JSON auswettung des http requests implementiert sdk min version auf 21 (5.0) geändert

--- app/src/main/java/com/hsowl/seta/data/SmartMeter.java
+++ app/src/main/java/com/hsowl/seta/data/SmartMeter.java
@@ -1,5 +1,8 @@
 package com.hsowl.seta.data;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 public class SmartMeter {
     private String host;
 
@@ -12,6 +15,19 @@ public class SmartMeter {
 
     public SmartMeterData requestData(){
         SmartMeterData smartMeterData = null;
+        String response = null;
+
+        //TODO http request to Smartmeter
+
+        //reformat json response
+        response = response.replace("1-0:2.4.0*255","activePowerPos");
+        response = response.replace("1-0:2.8.0*255", "activePowerNeg");
+        response = response.replace("serial", "\"serial");
+        response = response.replace(", ", ", \"");
+        response = response.replace(": ", "\": ");
+        //parse valid json into gson and get smartMeterData
+        Gson gson = new GsonBuilder().create();
+        smartMeterData = gson.fromJson(response, SmartMeterData.class);
 
         return smartMeterData;
     }
