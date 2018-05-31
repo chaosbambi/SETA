9b8a2227b34c0735a001b502aed43639255dc4ed
MSandeman <msandermann96@gmail.com>
2018-05-31 13:40:11 +0200

    Work in progress

--- app/src/main/java/com/hsowl/seta/ui/MainActivity.java
+++ app/src/main/java/com/hsowl/seta/ui/MainActivity.java
@@ -1,7 +1,10 @@
 package com.hsowl.seta.ui;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.hsowl.seta.R;
+import com.hsowl.seta.data.WeatherStation;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        WeatherStation ws = new WeatherStation(52.016859, 8.904493);
+        ws.getWeatherData();
     }
 }
