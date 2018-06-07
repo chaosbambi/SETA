480a1981f5f54900a8ceb60926c6f7fdb95961a0
Mario Kiese <mali.mk73@googlemail.com>
2018-06-07 17:11:19 +0200

    Aktuelles UI-Konstrukt (Activities, Fragments) - MainActivity - MainFragment - DeviceActivity - DeviceFragment - CustomSettingsActivity - CustomSettingsFragment - GraphPastForecastActivity - GraphPastForecastFragment - GraphPastSmartmeterDataActivity - GraphPastSmartmeterDataFragment - HelpActivity - HelpFragment - AboutActivity - AboutFragment
    - zu oben genannten Daten die entsprechenden xml-Dateien
    - menu
    - colors
    - styles

--- app/src/main/java/com/hsowl/seta/ui/MainActivity.java
+++ app/src/main/java/com/hsowl/seta/ui/MainActivity.java
@@ -1,13 +1,42 @@
 package com.hsowl.seta.ui;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.ScrollView;
+import android.widget.TextView;
+
+import com.hsowl.seta.R;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Variables
+    ScrollView svTrafficLights;
+    ImageView ivTrafficLight0; //TODO Adapter/Liste nutzen
+    TextView tvHeader;
+    TextView tvContent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Wir prüfen, ob Menü-Element mit der ID "action_daten_aktualisieren"
+        // ausgewählt wurde und geben eine Meldung aus
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
 }
