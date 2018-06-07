40d8afa520ea55a5e79388e13eabe5aafb075537
Mario Kiese <mali.mk73@googlemail.com>
2018-06-07 17:22:59 +0200

    - Delete menu options from fragments

---
+++ app/src/main/java/com/hsowl/seta/ui/GraphPastSmartmeterDataActivity.java
@@ -0,0 +1,31 @@
+package com.hsowl.seta.ui;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.hsowl.seta.R;
+
+public class GraphPastSmartmeterDataActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_graph_past_smartmeter_data);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Wir prüfen, ob Menü-Element mit der ID "action_daten_aktualisieren"
+        // ausgewählt wurde und geben eine Meldung aus
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/hsowl/seta/ui/MainFragment.java
@@ -0,0 +1,20 @@
+package com.hsowl.seta.ui;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.hsowl.seta.R;
+
+public class MainFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        return rootView;
+    }
+
+}
