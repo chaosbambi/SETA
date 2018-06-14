4302b1dd0c59f154cfe6054eb20d7d4ccde85a19
MSandeman <msandermann96@gmail.com>
2018-06-14 16:32:33 +0200

    Added Acync task for sending an http request to the smart meter and parse it into a SmartMeterData object Added a method and attribute to build the url for get requests in the SmartMeter class

--- app/src/main/java/com/hsowl/seta/data/SmartMeter.java
+++ app/src/main/java/com/hsowl/seta/data/SmartMeter.java
@@ -1,5 +1,8 @@
 package com.hsowl.seta.data;
 
+import android.os.AsyncTask;
+import android.util.Log;
+
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
@@ -12,24 +15,60 @@ public class SmartMeter {
 
     private String cookie;
 
+    private final String baseURL = "http://";
+
 
     public SmartMeterData requestData(){
         SmartMeterData smartMeterData = null;
         String response = null;
 
-        //TODO http request to Smartmeter
+        String url = buildDataGetURL();
 
-        //reformat json response
-        response = response.replace("1-0:2.4.0*255","activePowerPos");
-        response = response.replace("1-0:2.8.0*255", "activePowerNeg");
-        response = response.replace("serial", "\"serial");
-        response = response.replace(", ", ", \"");
-        response = response.replace(": ", "\": ");
-        //parse valid json into gson and get smartMeterData
-        Gson gson = new GsonBuilder().create();
-        smartMeterData = gson.fromJson(response, SmartMeterData.class);
+        //TODO http request to Smartmeter
+        JSONSmartMeterTask task = new JSONSmartMeterTask();
+        try{
+            smartMeterData = task.execute(url).get();
+        }catch(Throwable t){
+            Log.getStackTraceString(t);
+        }
 
         return smartMeterData;
+    }
+
+    private String buildDataGetURL(){
+        return baseURL.concat(host + "/data.php");
+    }
+
+
+    /**
+     * This inner class is used to execute the API request on a different Thread and get the
+     * SmartMeterData object from the JSON response
+     */
+    private static class JSONSmartMeterTask extends AsyncTask<String, Void, SmartMeterData> {
+
+        @Override
+        protected SmartMeterData doInBackground(String... strings) {
+            SmartMeterData smd = null;
+            String data = ((new HttpClient().getData(strings[0])));
+
+            //reformat json response
+            data = data.replace("1-0:2.4.0*255","activePowerPos");
+            data = data.replace("1-0:2.8.0*255", "activePowerNeg");
+            data = data.replace("serial", "\"serial");
+            data = data.replace(", ", ", \"");
+            data = data.replace(": ", "\": ");
+
+            //Parse the JSON response in a class of the WheaterData type
+            try {
+                Gson gson = new GsonBuilder().create();
+
+                smd = gson.fromJson(data, SmartMeterData.class);
+            }catch (Exception e){
+                Log.getStackTraceString(e);
+            }
+
+            return smd;
+        }
     }
 
 }
