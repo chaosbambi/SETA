9c5002f113dc12b15d6e9934965a65b7fd0c03ec
MSandeman <msandermann96@gmail.com>
2018-06-17 10:33:05 +0200

    Added a Unit-Test for the PV-Prognosis class,however not executed yet

--- app/src/main/java/com/hsowl/seta/logic/PvPrognosis.java
+++ app/src/main/java/com/hsowl/seta/logic/PvPrognosis.java
@@ -1,9 +1,11 @@
 package com.hsowl.seta.logic;
 
+import android.util.Log;
+
 import java.util.Calendar;
 import java.util.Date;
 
-class PvPrognosis {
+public class PvPrognosis {
     private SunPosition sunPosition;
     private double pvPeakPower;
     private double pvGrossPower;
@@ -12,7 +14,7 @@ class PvPrognosis {
     private double azimuth;
     private double slope;
 
-    PvPrognosis(double pvPeakPower, double lat, double lon, double azimuth, double slope){
+    public PvPrognosis(double pvPeakPower, double lat, double lon, double azimuth, double slope){
         this.sunPosition = new SunPosition(lat, lon, azimuth, slope);
         this.pvPeakPower = pvPeakPower;
         this.pvGrossPower = calculateGrossPower(pvPeakPower);
@@ -22,6 +24,13 @@ class PvPrognosis {
         this.slope = slope;
     }
 
+    /**
+     * This Method fills the given prognosis Array with estimated values of the pv output depending
+     * on the day of the year, coordinates, azimuth, slope an the weatherFactor
+     * @param prognosis         Array to fill with prognosis data
+     * @param weatherFactor     Factor between 0 and 100
+     * @param day               Day of the Year for prognosis
+     */
     public void calculatePvPrognosis(double[] prognosis, double[] weatherFactor, Date day){
 
         //Preparations
@@ -57,18 +66,29 @@ class PvPrognosis {
                 factor[i] = 0;
         }
 
-        for (int j = 0; j < prognosis.length; j++){
-            //Calculate optimal outcome
-            double optOutcome = pvGrossPower * radiationIntensity * factor [j];
+        try{
+            for (int j = 0; j < prognosis.length; j++){
+                //Calculate optimal outcome
+                double optOutcome = pvGrossPower * radiationIntensity * factor [j];
 
-            //Outcome with usage of weather information
-            double totalOutcome = optOutcome * ((11.0/1000.0 * Math.pow(weatherFactor[j], 2) - 41.0/20.0 * weatherFactor[j] + 200.0) /100.0);
+                if (weatherFactor[j] > 100){
+                    weatherFactor[j] = 100;
+                }else if(weatherFactor[j] < 0){
+                    weatherFactor[j] = 0;
+                }
 
-            if (totalOutcome > pvPeakPower){
-                prognosis[j] = pvPeakPower;
-            }else {
-                prognosis[j] = totalOutcome;
+                //Outcome with usage of weather information
+                double totalOutcome = optOutcome * ((11.0/1000.0 * Math.pow(weatherFactor[j], 2) - 41.0/20.0 * weatherFactor[j] + 200.0) /100.0);
+
+                if (totalOutcome > pvPeakPower){
+                    prognosis[j] = pvPeakPower;
+                }else {
+                    prognosis[j] = totalOutcome;
+                }
             }
+        }catch (Exception e){
+            Log.getStackTraceString(e);
+            throw e;
         }
     }
 
---
+++ app/src/test/java/com/hsowl/seta/ui/PvPrognosisTest.java
@@ -0,0 +1,35 @@
+package com.hsowl.seta.ui;
+
+import com.hsowl.seta.logic.PvPrognosis;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Date;
+
+public class PvPrognosisTest {
+
+    @Test
+    public void testCalculatePvPrognosis(){
+        double [] prog = new double[24];
+        double [] wfac = new double[24];
+        PvPrognosis pp = new PvPrognosis(5000.0,52.026673,8.901862, 180,45);
+
+        //Values were extracted from the Matlab-Script that was used to design the tested method
+        double[] expected = {0,0,0,0,0,0,0,741.538406950969,1844.94137071594,2944.06107028363,
+                3882.66887665735,4550.99252090546,4878.17117563033,4831.07146465736,4414.40922449884,
+                3670.71027714868,2680.29357490889,1563.32862936981,490.779500531811,0,0,0,0,0};
+
+        Date d = new Date();
+        d.setDate(28);
+        d.setMonth(5);
+        d.setYear(2018);
+        d.setSeconds(0);
+        d.setMinutes(0);
+        d.setHours(0);
+
+        pp.calculatePvPrognosis(prog,wfac,d);
+
+        Assert.assertArrayEquals(expected, prog,20.0);
+    }
+}
