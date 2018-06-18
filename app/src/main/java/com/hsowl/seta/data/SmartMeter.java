5136b280378f45f62e95c6ce950258d498056d36
MSandeman <msandermann96@gmail.com>
2018-06-18 12:50:36 +0200

    Added a method to make an http request with the start.php uml to check whether the user is authenticated. Added a dummy method for authentication. Refectoring of attributes. Added an authentication attribute to the SmartMeterData class.

--- app/src/main/java/com/hsowl/seta/data/SmartMeter.java
+++ app/src/main/java/com/hsowl/seta/data/SmartMeter.java
@@ -21,22 +21,6 @@ public class SmartMeter {
         this. host = host;
     }
 
-    public SmartMeterData requestData(){
-        SmartMeterData smartMeterData = null;
-        String response = null;
-
-        String url = buildDataGetURL();
-
-        //TODO http request to Smartmeter
-        JSONSmartMeterTask task = new JSONSmartMeterTask();
-        try{
-            smartMeterData = task.execute(url).get();
-        }catch(Throwable t){
-            Log.getStackTraceString(t);
-        }
-
-        return smartMeterData;
-    }
 
     private String buildDataGetURL(){
         return baseURL.concat(host + "/mum-webservice/data.php");
@@ -45,6 +29,61 @@ public class SmartMeter {
         return baseURL.concat(host + "/start.php");
     }
 
+    /**
+     * This method delegates a task to send an http request to the host address to check whether the
+     * user is authenticated.
+     * @return  true if the JSON paramter in the response equals true. Otherwise returns false.
+     */
+    public boolean checkAuthentication(){
+        SmartMeterData smartMeterData = null;
+
+        String startUrl = buildStartGetURL();
+
+        JSONSmartMeterTask task = new JSONSmartMeterTask();
+        try{
+            smartMeterData = task.execute(startUrl).get();
+        }catch(Throwable t){
+            Log.getStackTraceString(t);
+        }
+
+        if (smartMeterData != null) {
+            return (smartMeterData.getAuthentication().equals("true"));
+        }
+
+        //TODO: Check if an exception should be implemented that will be thown if the smartMeterData object is null
+
+        return false;
+    }
+
+    /**
+     * Not implemented yet.
+     * @return  false
+     */
+    public boolean authenticate(){
+        //TODO: In further releases a method for the Login of the SmartMeter with pw is needed
+        return false;
+    }
+
+    /**
+     * This Method delegates a Task to send an http request to the host address and returns the
+     * parsed SmartMeterData object from the response.
+     * @return  A SmartMeterData object with recent data from the host address or null if the
+     * request failed
+     */
+    public SmartMeterData requestData(){
+        SmartMeterData smartMeterData = null;
+
+        String dataUrl = buildDataGetURL();
+
+        JSONSmartMeterTask task = new JSONSmartMeterTask();
+        try{
+            smartMeterData = task.execute(dataUrl).get();
+        }catch(Throwable t){
+            Log.getStackTraceString(t);
+        }
+
+        return smartMeterData;
+    }
 
     /**
      * This inner class is used to execute the API request on a different Thread and get the
