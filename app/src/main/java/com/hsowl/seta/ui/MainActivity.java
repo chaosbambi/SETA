252733e95fd1932ebd3c5dc78af79f72657df1a0
MSandeman <msandermann96@gmail.com>
2018-07-01 10:03:23 +0200

    Cleaned MainActivity from code for testing

--- app/src/main/java/com/hsowl/seta/ui/MainActivity.java
+++ app/src/main/java/com/hsowl/seta/ui/MainActivity.java
@@ -4,8 +4,6 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 import com.hsowl.seta.R;
-import com.hsowl.seta.data.SmartMeter;
-import com.hsowl.seta.data.SmartMeterData;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,13 +11,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        SmartMeter sm = new SmartMeter("sem100");
-        SmartMeterData smd = null;
-        if(sm.checkAuthentication()){
-            smd = sm.requestData();
-        }
-
-        smd.getStatus();
     }
 }
